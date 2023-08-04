1e2d78a917f9fbafd8d64ca4d32b614be50be7a0
utkarshbankar <utkar7hbankar@gmail.com>
2023-08-04 23:05:06 +0530

    added the code to call external schematics from exsisting one

--- schm-first/src/schm-first/index.ts
+++ schm-first/src/schm-first/index.ts
@@ -1,13 +1,14 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, schematic } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function schmFirst(_options: any): Rule {
   
-  console.log('Hello from your new schematic!');
+  console.log('Hello from your new first schematic!');
   
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+  return (_tree: Tree, _context: SchematicContext) => {
+    const rule = schematic("child-schem", _options);
+    return rule;
   };
 }
