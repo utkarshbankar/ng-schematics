8801dd5e97789060f632a8122848e8eba7bf4e35
utkarshbankar <utkar7hbankar@gmail.com>
2023-08-26 21:21:09 +0530

    this is raw schematic for all our use cases just refer document

--- schm-first/src/lazy-module/index.ts
+++ schm-first/src/lazy-module/index.ts
@@ -1,21 +1,12 @@
 import { normalize, strings, } from '@angular-devkit/core';
 import { MergeStrategy, Rule, SchematicContext, SchematicsException, Tree, apply, applyTemplates, chain, externalSchematic, mergeWith, move, url } from '@angular-devkit/schematics';
-// import { RunSchematicTask } from '@angular-devkit/schematics/tasks';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
 import { existsSync, readdirSync, unlinkSync } from 'fs';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-/***
- * problem stmt: that we are creating our own module from template due to we do not have the proper template
- * for module that is there is not decelerations and no bootstrapping component, we may add new child module
- * and it import in the root module but some missing imports are there so we are creating owr own template - done
- * only pending part is adding router-outlet inside the container.component.html
- *  
-*/
-
-export function mfApp(_options: any): Rule {
+export function lazyModule(_options: any): Rule {
   return async (_tree: Tree, _context: SchematicContext) => {
 
     const workspace = await getWorkspace(_tree);
@@ -33,8 +24,6 @@ export function mfApp(_options: any): Rule {
     }
 
     const projectName = workspace.projects.get(_options.project);
-    // console.log("projectName", projectName?.extensions?.projectType);
-
     const projectType = projectName?.extensions?.projectType === 'application' ? 'app' : 'lib';
 
     _options.path = `${project.sourceRoot}/${projectType}`;
@@ -44,15 +33,8 @@ export function mfApp(_options: any): Rule {
     /**
      * The code that I am writing here will delete existing files from app folder and add the new module and lazy module in the app folder
     */
-    // this will read the available files from the directory
-    // readdirSync(_options.path).forEach(file => {
-    //   console.log(file);
-    // });
 
     if (_options.path && existsSync(_options.path.toString())) {
-
-      // console.log("main path of app", _options.path);
-
       readdirSync(_options.path.toString()).forEach(file => {
         if (file.includes('app')) {
           //  console.log(`./${_options.path}/${file}`);
@@ -63,63 +45,49 @@ export function mfApp(_options: any): Rule {
         }
       });
 
-      // console.log("current directory path is", _options.path);
-
-    } else {
-      // true ?? addImportToNgModule(_options);
-      //  _options.path = `${_options.path}/container`;
-      console.error("inside the else of log");
-
     }
     /**
      * above code is common in every schematic file this is just to find the type of app and
      * path  from here onwards we have basic checks
     */
-    //  console.log("_options_options before calling task", _options);
-
-    // _context.addTask(new RunSchematicTask('build-app', _options));
 
     if (_options.path) {
-      // console.log("when path id available import have error", _options.path);    
-      const templateSourceModule = apply(url('./files'), [
-        applyTemplates({
-          ..._options,
-          classify: strings.classify,
-          dasherize: strings.dasherize,
-        }),
-        move(normalize(_options.path as string))
-      ]);
+      // const templateSourceModule = apply(url('./files'), [
+      //   applyTemplates({
+      //     ..._options,
+      //     classify: strings.classify,
+      //     dasherize: strings.dasherize,
+      //   }),
+      //   move(normalize(_options.path as string))
+      // ]);
+
       return chain([
-        externalSchematic('@schematics/angular', 'module', {
-          name: _options.name,
-          flat: true,
-          routing: _options.routing,
-          routingScope: 'Root',
-          path: _options.path,
-          project: _options.name,
-        }),
-        externalSchematic('@schematics/angular', 'component', {
-          name: _options.name,
-          path: _options.path,
-          flat: true
-        }),
-        externalSchematic('@schematics/angular', 'module', {
-          name: _options.lazyModuleName,
-          flat: true,
-          commonModule: true,
-          path: `${_options.path}/${_options.lazyModuleName}`,
-          route: 'make_this_string_empty',
-          module: `${_options.path}/${_options.name}.module`,
-          routing: _options.routing,
-          routingScope: 'Child',
-        }),
-        externalSchematic('@schematics/angular', 'component', {
-          name: _options.lazyModuleName,
-          path: `${_options.path}/${_options.lazyModuleName}`,
-          flat: true
-        }),
-        mergeWith(templateSourceModule, MergeStrategy.Overwrite),
-        // mergeWith(templateSourceComponent, MergeStrategy.Overwrite)
+        rootModuleRule(_options),
+        lazyModuleRule(_options)
+        // externalSchematic('@schematics/angular', 'module', {
+        //   name: _options.name,
+        //   flat: true,
+        //   path: _options.path,
+        //   project: _options.name,
+        // }),
+        // externalSchematic('@schematics/angular', 'component', {
+        //   name: _options.name,
+        //   path: _options.path,
+        //   flat: true
+        // }),
+        // externalSchematic('@schematics/angular', 'module', {
+        //   name: _options.lazyModuleName,
+        //   flat: true,
+        //   commonModule: true,
+        //   path: `${_options.path}/${_options.lazyModuleName}`,
+        //   module: `${_options.path}/${_options.name}.module`
+        // }),
+        // externalSchematic('@schematics/angular', 'component', {
+        //   name: _options.lazyModuleName,
+        //   path: `${_options.path}/${_options.lazyModuleName}`,
+        //   flat: true
+        // }),
+        // mergeWith(templateSourceModule, MergeStrategy.Overwrite),
       ]);
     } else {
       // this will be used by the schematics command
@@ -138,4 +106,67 @@ export function mfApp(_options: any): Rule {
       ]);
     }
   };
+}
+
+export function rootModuleRule(_options: any): Rule {
+  return async (_tree: Tree, _context: SchematicContext) => {
+
+    if (_options.path) {
+      const templateSourceRootModule = apply(url('./files/root'), [
+        applyTemplates({
+          ..._options,
+          classify: strings.classify,
+          dasherize: strings.dasherize,
+        }),
+        move(normalize(_options.path as string))
+      ]);
+
+      return chain([
+        externalSchematic('@schematics/angular', 'module', {
+          name: _options.name,
+          flat: true,
+          path: _options.path
+        }),
+        externalSchematic('@schematics/angular', 'component', {
+          name: _options.name,
+          path: _options.path,
+          flat: true
+        }),
+        mergeWith(templateSourceRootModule, MergeStrategy.Overwrite)
+      ])
+    }
+  }
+}
+
+export function lazyModuleRule(_options: any): Rule {
+  return async (_tree: Tree, _context: SchematicContext) => {
+      
+    if (_options.path) {
+      console.log("..._options", _options);
+      
+      const templateSourceLazyModule = apply(url('./files/lazy'), [
+        applyTemplates({
+          ..._options,
+          classify: strings.classify,
+          dasherize: strings.dasherize,
+        }),
+        move(`${_options.path}/${_options.lazyModuleName}` as string)
+      ]);
+
+      return chain([
+        externalSchematic('@schematics/angular', 'module', {
+          name: _options.lazyModuleName,
+          flat: true,
+          commonModule: true,
+          path: `${_options.path}/${_options.lazyModuleName}`
+        }),
+        externalSchematic('@schematics/angular', 'component', {
+          name: _options.lazyModuleName,
+          path: `${_options.path}/${_options.lazyModuleName}`,
+          flat: true
+        }),
+        mergeWith(templateSourceLazyModule, MergeStrategy.Overwrite)
+      ])
+    }
+  }
 }
